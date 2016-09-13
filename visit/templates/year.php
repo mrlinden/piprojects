e86426040adcf718ebf5805224e3afbf2fa6489c
erammle <erammle@SE00028611.ericsson.se>
2016-09-13 23:20:30 +0200

    .....

--- visit/templates/year.php
+++ visit/templates/year.php
@@ -21,6 +21,17 @@
   var datadivWidth = 1000;
   var datadivHeight = (30 + (pixelsPerWeek * nrYearsToShow));
 
+  function twoDigits(d) {
+      if(0 <= d && d < 10) return "0" + d.toString();
+      if(-10 < d && d < 0) return "-0" + (-1*d).toString();
+      return d.toString();
+  }
+
+  Date.prototype.toMysqlFormat = function() {
+      return this.getUTCFullYear() + "-" + twoDigits(1 + this.getUTCMonth()) + "-" + twoDigits(this.getUTCDate());
+  };
+
+
     function drawChart() {
       var dataTable = new google.visualization.DataTable();
       dataTable.addColumn({ type: 'date', id: 'Datum' });
@@ -69,7 +80,8 @@
         var selectedItem = chart.getSelection()[0];
         if (selectedItem) {
           var selectedDate = new Date(selectedItem.date);
-          window.location.search = '?y=' + selectedDate.getFullYear() + "&m=" + (selectedDate.getMonth() + 1 ) + "&d=" + selectedDate.getDate();
+          window.location.search = '?d=' + selectedDate.toMysqlFormat();
+          //window.location.search = '?y=' + selectedDate.getFullYear() + "&m=" + (selectedDate.getMonth() + 1 ) + "&d=" + selectedDate.getDate();
         }
       }
 
