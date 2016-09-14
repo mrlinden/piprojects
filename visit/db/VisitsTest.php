403127f1869b5dae0f75d2a6f037f5c7a8fc14b2
jimma <jimma@LAPTOP-3IEHIAHQ>
2016-09-15 01:19:51 +0200

    x

--- visit/db/VisitsTest.php
+++ visit/db/VisitsTest.php
@@ -11,16 +11,15 @@ $visits = new Cupolen\Visits($db);
 // Get the list of visits
 $dayList = $visits->getVisitsPerDay();
 
-foreach ($dayList as $row):
+foreach ($dayList as $row) {
   	print "Date : " . $row['date'] . " had " . $row['visits'] . " visits \n" ;
 
 	// Get the list of visits for each day
 	$minuteList = $visits->getVisitsPerMinute($row['date']);
 	
-	foreach ($minuteList as $row2):
+	foreach ($minuteList as $row2) {
 		print " - Time : " . $row2['intervalStart'] . " had " . $row2['doorA'] . " visits \n" ;
-	endforeach;
-endforeach;
-
+	}
+}
 
 ?>
