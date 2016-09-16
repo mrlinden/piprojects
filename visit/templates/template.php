af954724c5725e61f403e1b6105e1b647f8ac5c7
jimma <jimma@LAPTOP-3IEHIAHQ>
2016-09-16 23:41:03 +0200

    fd

--- visit/templates/template.php
+++ visit/templates/template.php
@@ -4,21 +4,8 @@
     <title><?=$this->e($title)?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<style>
-	h1 {
 
-	}
-	h2 {
-	    color: #000000;
-	    font-family: Arial,Helvetica Neue,Helvetica,sans-serif;
-		font-size: 18px;
-		font-style: normal;
-		font-variant: normal;
-		font-weight: 500;
-		line-height: 26.4px;
-	    margin-left: 40px;
-	}
-	
-	#footer {
+	.footer {
 	    color: #000000;
 	    font-family: Arial,Helvetica Neue,Helvetica,sans-serif;
 		font-size: 8px;
@@ -28,7 +15,7 @@
 	    text-align: right;
 	}
 
-	#space {
+	.space {
 		height:10px;
 		font-family: Arial,Helvetica Neue,Helvetica,sans-serif;
 	    font-size: 10px;
@@ -37,12 +24,7 @@
 	    padding: 5px;
 	}
 	
-	#logo {
-		margin: auto;
-		padding: 5px;
-	}
-	
-	#titel {
+	.titel {
 	    padding: 5px;
 	  	color: #FFFFFF;
 	    font-family: Arial,Helvetica Neue,Helvetica,sans-serif;
@@ -51,32 +33,26 @@
 		font-variant: normal;
 		font-weight: 500;
 		line-height: 40px;
-		border-color: red;
-		border-width: 2px;
-	}
-
-	#titel2 {
-	    padding: 5px;
-	  	color: #FFFFFF;
-	    font-family: Arial,Helvetica Neue,Helvetica,sans-serif;
-		font-size: 10px;
-		font-style: normal;
-		font-variant: normal;
-		font-weight: 500;
-		line-height: 10px;
-		border-color: red;
-		border-width: 2px;
+		vertical-align: middle;
+        display: table-cell;
 	}
 	
-	#grad1 {
+	.logo {
+	    vertical-align: middle;
+	    display: table-cell;
+	    margin-right: 1em;
+	    width:44px; 
+	    height: 50px;
+    }
+    
+	.grad {
 	    height: 60px;
-	    background: red; /* For browsers that do not support gradients */
+	    background: #44276B; /* For browsers that do not support gradients */
 	    background: -webkit-linear-gradient(left, #44276B, #78649E); /* For Safari 5.1 to 6.0 */
 	    background: -o-linear-gradient(right, #44276B, #78649E); /* For Opera 11.1 to 12.0 */
 	    background: -moz-linear-gradient(right, #44276B, #78649E); /* For Firefox 3.6 to 15 */
 	    background: linear-gradient(to right, #44276B, #78649E); /* Standard syntax (must be last) */
-	    margin: auto;
-		vertical-align: middle;
+	    display: table;
     }
     </style>
 
@@ -84,16 +60,18 @@
 	<?=$this->section('content')?>
 </head>
 <body>
-    <div id="grad1">
-    <div id="logo"><img alt="Cupolen" src="Logo.png" style="width:44px; height: 50px;"></div>
-    <div id="titel">&nbsp;Bes&ouml;ksr&auml;knare</div>
-    <div id="titel2">In- och ut-passager genom entre-d&ouml;rrarna och d&ouml;rren från innerg&aring;rden.<BR>Notera att ökalendern </div>
+    <div class="grad">
+    <span class="titel"><img alt="Cupolen" src="Logo.png" class="logo"></span>
+	<span class="titel">Bes&ouml;ksr&auml;knare
+	<br>In- och ut-passager genom entre-d&ouml;rrarna och d&ouml;rren från innerg&aring;rden.
+	</span>
     </div>
-    <div id="space">In- och ut-passager genom entre-d&ouml;rrarna och d&ouml;rren från innerg&aring;rden.</div>
+
+    <div class="space">
     <div id="datadiv" style="border: 0px; height: 100px; width: 100px;"></div>
-    <div id="space"></div>
+    <div class="space"></div>
     
 <hr>
-<div id="footer">Copyright Marcus Lind&eacute;n, Teknikgruppen 2016</div>
+<div class="footer">Copyright Marcus Lind&eacute;n, Teknikgruppen 2016</div>
 </body>
 </html>
