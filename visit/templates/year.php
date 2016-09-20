b0d928dae77b48964dd7e335947898dcc4cb76d1
jimma <jimma@LAPTOP-3IEHIAHQ>
2016-09-21 00:17:54 +0200

    mm

--- visit/templates/year.php
+++ visit/templates/year.php
@@ -38,24 +38,7 @@
       var dataTable = new google.visualization.DataTable();
       dataTable.addColumn({ type: 'date', id: 'Datum' });
       dataTable.addColumn({ type: 'number', id: 'In/ut-passager' });
-      dataTable.addRows([
-        [ new Date(2012, 3, 13), 37032 ],
-        [ new Date(2012, 3, 14), 38024 ],
-        [ new Date(2012, 3, 15), 38024 ],
-        [ new Date(2012, 3, 16), 38108 ],
-        [ new Date(2012, 3, 17), 38229 ],
-        // Many rows omitted for brevity.
-        [ new Date(2013, 9, 4), 38177 ],
-        [ new Date(2013, 9, 5), 38705 ],
-        [ new Date(2013, 9, 12), 38210 ],
-        [ new Date(2013, 9, 13), 38029 ],
-        [ new Date(2013, 9, 19), 38823 ],
-        [ new Date(2013, 9, 23), 38345 ],
-        [ new Date(2013, 9, 24), 38 ],
-              [ new Date(2026, 9, 24), 38436 ],
-        [ new Date(2013, 9, 30), 38447 ]
-      ]);
-      //dataTable.addRows(dataRows);
+      dataTable.addRows(dataRows);
 
 	  var theDiv = document.getElementById('datadiv');
 	  theDiv.style.width  = "1000px";
