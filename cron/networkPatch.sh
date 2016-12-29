aa93df3f165ade8b13456249c30a8a42f299e274
jimma <jimma@LAPTOP-3IEHIAHQ>
2016-12-29 21:52:14 +0100

    ....

--- cron/networkPatch.sh
+++ cron/networkPatch.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # script that disabbles power management for wifi in case it is not already disabled.
-PWR_ALREADY_OFF = cat /etc/network/interfacesML | grep "wireless-power off"
 
 if grep -q wireless-power "/etc/network/interfacesML"; then
 	echo "nothing to be done"
 else
 	echo "to be done"
-	sudo echo "wireless-power off" >> /etc/network/interfacesML
-	#sudo /etc/init.d/networking restart
+	echo "wireless-power off" >> /etc/network/interfacesML
+	# /etc/init.d/networking restart
 fi
