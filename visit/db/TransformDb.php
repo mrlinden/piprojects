d3bbba8e36ba7a191ef1c88bb888f1d74103d27d
jimma <jimma@LAPTOP-3IEHIAHQ>
2017-03-18 23:24:44 +0100

    sddsd

--- visit/db/TransformDb.php
+++ visit/db/TransformDb.php
@@ -5,7 +5,7 @@ include 'Visits.php';
 $db = new PDO('mysql:host=localhost;dbname=visits;charset=utf8', 'root', 'linden1mysql');
 
 // Get the old content
-$sqlQuery = "SELECT * from `minutetable`";
+$sqlQuery = "SELECT UNIX_TIMESTAMP(intervalStop), dorA, doorB, doorC, doorD from `minutetable`";
 $old = $db->query($sqlQuery);
 
 foreach ($old as $row) {
@@ -13,7 +13,7 @@ foreach ($old as $row) {
 
 	$date = floor($row['intervalStop']/600)*600;
 	
-	print "floor " . $date;
+	print "floor " . $date . "\n";
 	
 }
 
