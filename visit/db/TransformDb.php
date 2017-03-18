5276c9d59a49b00233569747a1162c4029bf119c
jimma <jimma@LAPTOP-3IEHIAHQ>
2017-03-18 23:33:24 +0100

    dfdaf

--- visit/db/TransformDb.php
+++ visit/db/TransformDb.php
@@ -5,13 +5,13 @@ include 'Visits.php';
 $db = new PDO('mysql:host=localhost;dbname=visits;charset=utf8', 'root', 'linden1mysql');
 
 // Get the old content
-$sqlQuery = "SELECT UNIX_TIMESTAMP(intervalStop), doorA, doorB, doorC, doorD from `minutetable`";
+$sqlQuery = "SELECT UNIX_TIMESTAMP(intervalStop) AS date, doorA, doorB, doorC, doorD from `minutetable`";
 $old = $db->query($sqlQuery);
 
 foreach ($old as $row) {
-		print "date: " . $row['intervalStop'] . " A: " . $row['doorA'] . " B: " . $row['doorB'] . " C: " . $row['doorC'] . " D: " . $row['doorD'] . " visits \n" ;
+		print "date: " . $row['date'] . " A: " . $row['doorA'] . " B: " . $row['doorB'] . " C: " . $row['doorC'] . " D: " . $row['doorD'] . " visits \n" ;
 
-	$date = floor($row['intervalStop']/600)*600;
+	$date = floor($row['date']/600)*600;
 	
 	print "floor " . $date . "\n";
 	
