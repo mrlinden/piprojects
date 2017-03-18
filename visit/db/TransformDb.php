9ac9d714b513b951fa9543eca2ffe789109d9893
jimma <jimma@LAPTOP-3IEHIAHQ>
2017-03-19 00:00:35 +0100

    mmm

--- visit/db/TransformDb.php
+++ visit/db/TransformDb.php
@@ -16,28 +16,48 @@ foreach ($old as $row) {
 	$c = $row['doorC'];
 	$d = $row['doorD'];
 	
-	print "date: " . $row['date'] . " A: " . $row['doorA'] . " B: " . $row['doorB'] . " C: " . $row['doorC'] . " D: " . $row['doorD'] . " visits \n" ;
+	print "date: " . $date . " A: " . $a . " B: " . $b . " C: " . $c . " D: " . $d . " \n" ;
 	
-	print "floor " . $date . "\n";
-	
-	$sqlWrite = "INSERT INTO `visits`.`sensordata` (`date`, `id`, `count`) VALUES (%s,%s,%s)";
+	$sqlWrite = "INSERT INTO `visits`.`sensordata` (`date`, `id`, `count`) VALUES";
+	$addedItem = 0;
 	
 	if ($a != 0) {
+		$sqlWrite .= " (FROM_UNIXTIME(".$date."), 1, ".$a.")";
+		$addedItem += 1;
 		print "Write A" . $a;
 	}
 
 	if ($b != 0) {
+		if ($addedItem > 0) $sqlWrite .= ",\n";
+		$sqlWrite .= " (FROM_UNIXTIME(".$date."), 2, ".$b.")";
+		$addedItem += 1;
+		
 		print "Write B" . $b;
 	}
 
 	if ($c != 0) {
+		if ($addedItem > 0) $sqlWrite .= ",\n";
+		$sqlWrite .= " (FROM_UNIXTIME(".$date."), 3, ".$c.")";
+		$addedItem += 1;
+		
 		print "Write C" . $c;
 	}
 
 	if ($d != 0) {
+		if ($addedItem > 0) $sqlWrite .= ",\n";
+		$sqlWrite .= " (FROM_UNIXTIME(".$date."), 4, ".$d.")";
+		$addedItem += 1;
+		
 		print "Write D" . $d;
 	}
 	
+	print "SQL: " . $sqlWrite;
+	
+	#if ($db->query($sqlWrite) === TRUE) {
+	#	echo "New record created successfully";
+	#} else {
+#		echo "Error: " . $sql . "<br>" . $conn->error;
+#	}
 	
 }
 
