4e20b6bb12b2ab65de1eaae96295b7075c16dfcb
Marcus Linden <marcus.linden@vaderstad.com>
2019-11-08 19:23:13 +0100

    cont...

--- cupolen/www/sendHelvarTCPCommand.php
+++ cupolen/www/sendHelvarTCPCommand.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 $cmd = $_POST["cmd"];
-$result = `./sendHelvarTCPCommandLine.pl $cmd`;
+$result = `php ./sendHelvarTCPCommandLine.php $cmd`;
 echo json_encode($result);
 ?>
--- cupolen/www/sendHelvarUDPCommand.php
+++ cupolen/www/sendHelvarUDPCommand.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 $cmd = $_POST["cmd"];
-$result = `./sendHelvarUDPCommandLine.pl $cmd`;
+$result = `php ./sendHelvarUDPCommandLine.php $cmd`;
 echo json_encode($result);
 ?>
