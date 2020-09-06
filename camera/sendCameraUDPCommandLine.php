8d08a6e521f0c683bc36dfa47876492da1563c9d
mrlinden <jim.marcus.linden@gmail.com>
2020-09-06 16:06:06 +0200

    upd

--- camera/sendCameraUDPCommandLine.php
+++ camera/sendCameraUDPCommandLine.php
@@ -8,9 +8,9 @@ $addressLSB = 12 + $camNr;
 $address = '192.168.14.' . $addressLSB;
 $port = 5002;
 
-$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-$len = strlen($cmd);
-socket_sendto($sock, $cmd, $len, 0, $address, $port);
+$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_connect($sock, $address, $port);
+socket_write($sock, $cmd);
 socket_close($sock);
 
 echo "Ok ";
