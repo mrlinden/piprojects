e131f15d3ab8c329bf8b90f0997e018deda1d4f0
Marcus Linden <marcus.linden@vaderstad.com>
2020-09-06 13:07:04 +0200

    Upd

--- camera/sendCameraUDPCommandLine.php
+++ camera/sendCameraUDPCommandLine.php
@@ -5,7 +5,7 @@ $camNr = $argv[1];
 $cmd = hex2bin($argv[2]);
 
 $addressLSB = 12 + $camNr;
-$address = '192.168.14.' . addressLSB;
+$address = '192.168.14.' . $addressLSB;
 $port = 5002;
 
 $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
@@ -13,5 +13,5 @@ $len = strlen($cmd);
 socket_sendto($sock, $cmd, $len, 0, $address, $port);
 socket_close($sock);
 
-echo "Ok " . $cmd;
+echo "Ok ";
 ?>
