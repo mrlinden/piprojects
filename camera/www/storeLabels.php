aabeba68281a56527575dcec6b4bbd537192f115
mrlinden <jim.marcus.linden@gmail.com>
2020-10-02 23:43:46 +0200

    A

--- camera/www/storeLabels.php
+++
@@ -1,6 +0,0 @@
-<?php
-$ini = parse_ini_file('labels.ini');
-
-echo "let presetLabel = [ [\"", $ini['L11'], "\",\"", $ini['L12'], "\",\"", $ini['L13'], "\",\"", $ini['L14'], "\",\"", $ini['L15'], "\",\"", $ini['L16'], "\",\"", $ini['L17'], "\"]";
-
-?>
